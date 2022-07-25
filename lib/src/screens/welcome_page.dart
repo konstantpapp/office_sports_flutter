7e98f127b6d78da4e6d80b5c4260c993d0f15441
Konstantinos <konstantinos.pappas@tietoevry.com>
2022-07-25 10:55:22 +0200

    Draft design welcome page

--- lib/src/screens/welcome_page.dart
+++ lib/src/screens/welcome_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../shared/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
@@ -8,14 +10,67 @@ class WelcomePage extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     User? result = FirebaseAuth.instance.currentUser;
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Constants.primaryColor,
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.dark,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/app_icon.png"),
+              RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(children: <TextSpan>[
+                    TextSpan(
+                        text: 'Welcome to\n',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                        )),
+                    TextSpan(
+                        text: ' Office Sports!',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30.0)),
+                    TextSpan(
+                        text: ' 😎',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30.0)),
+                  ])),
+              SizedBox(height: size.height * 0.1),
+              const Text(
+                '🏓 - ⚽️',
+                style: TextStyle(fontSize: 40),
+              ),
+              SizedBox(height: size.height * 0.1),
+              SizedBox(
+                width: size.width * 0.7,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      side: MaterialStateProperty.all<BorderSide>(
+                          BorderSide.none)),
+                  child: const Text(
+                    'Sign in with Google',
+                    style: TextStyle(color: Constants.primaryColor),
+                  ),
+                ),
+              ),
+              SizedBox(height: size.height * 0.1),
+              FutureBuilder<PackageInfo>(
+                future: PackageInfo.fromPlatform(),
+                builder: (context, snapshot) {
+                  return Text(
+                    snapshot.hasData ? "Version ${snapshot.data!.version}" : "",
+                    style: const TextStyle(color: Colors.grey),
+                  );
+                },
+              ),
             ],
           ),
         ),
--- lib/src/shared/constants.dart
+++ lib/src/shared/constants.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/material.dart';
 
+class Constants {
+  // colors
+  static const primaryColor = Color.fromARGB(255, 75, 63, 236);
+}
