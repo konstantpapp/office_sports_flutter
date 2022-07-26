f38992fa0819fa5235b0452459a3b0b401a153a6
Konstantinos <konstantinos.pappas@tietoevry.com>
2022-07-26 17:35:29 +0200

    Pick emoji

--- lib/src/modals/profile_modal.dart
+++ lib/src/modals/profile_modal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../mixins/validation_mixin.dart';
 import '../shared/constants.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 
 class ProfileModal extends StatefulWidget {
   @override
@@ -12,7 +13,7 @@ class ProfileModal extends StatefulWidget {
 class ProfileModalState extends State<ProfileModal> with ValidationMixin {
   final formKey = GlobalKey<FormState>();
   String nickname = '';
-  String emoji = '';
+  String emoji = '🙂';
 
   @override
   void initState() {
@@ -35,78 +36,123 @@ class ProfileModalState extends State<ProfileModal> with ValidationMixin {
       isScrollControlled: true,
       enableDrag: false,
       builder: (BuildContext context) {
-        return Padding(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Container(
-            color: Constants.primaryColor,
-            height: size.height * 0.95,
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  SizedBox(height: size.height * 0.05),
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: const TextSpan(
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: 'Update profile\n\n',
-                            style: TextStyle(
-                              color: Constants.primaryTextColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 26.0,
-                            )),
-                        TextSpan(
-                            text: 'Choose a nickname and an associated\n',
-                            style: TextStyle(
+        return StatefulBuilder(builder: (context, setState) {
+          return Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              color: Constants.primaryColor,
+              height: size.height * 0.95,
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: size.height * 0.05),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Create profile\n\n',
+                              style: TextStyle(
                                 color: Constants.primaryTextColor,
-                                fontSize: 18.0)),
-                        TextSpan(
-                            text: 'emoji that people can remember you by.\n\n',
-                            style: TextStyle(
-                                color: Constants.primaryTextColor,
-                                fontSize: 18.0)),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.orange,
-                      border: Border.all(
-                        width: 5,
-                        color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 26.0,
+                              )),
+                          TextSpan(
+                              text: 'Choose a nickname and an associated\n',
+                              style: TextStyle(
+                                  color: Constants.primaryTextColor,
+                                  fontSize: 18.0)),
+                          TextSpan(
+                              text: 'emoji that people can remember you by.\n',
+                              style: TextStyle(
+                                  color: Constants.primaryTextColor,
+                                  fontSize: 18.0)),
+                        ],
                       ),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black,
-                          blurRadius: 2.0,
-                          spreadRadius: 2.0,
-                          offset: Offset(5.0, 5.0),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color.fromARGB(255, 143, 229, 240),
+                        border: Border.all(
+                          width: 5,
+                          color: Colors.white,
                         ),
-                      ],
-                    ),
-                    alignment: Alignment.center,
-                    width: 100,
-                    height: 100,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.emoji_emotions,
-                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 2.0,
+                            spreadRadius: 2.0,
+                            offset: Offset(5.0, 5.0),
+                          ),
+                        ],
                       ),
-                      onPressed: () {},
+                      alignment: Alignment.center,
+                      width: 100,
+                      height: 100,
+                      child: GestureDetector(
+                        child:
+                            Text(emoji, style: const TextStyle(fontSize: 50.0)),
+                        onTap: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return EmojiPicker(
+                                onEmojiSelected:
+                                    (Category category, Emoji selectedEmoji) {
+                                  setState(() {
+                                    emoji = selectedEmoji.emoji;
+                                    Navigator.pop(context);
+                                  });
+                                },
+                                config: const Config(
+                                    columns: 7,
+                                    // Issue: https://github.com/flutter/flutter/issues/28894
+                                    emojiSizeMax: 32,
+                                    verticalSpacing: 0,
+                                    horizontalSpacing: 0,
+                                    gridPadding: EdgeInsets.zero,
+                                    initCategory: Category.RECENT,
+                                    bgColor: Color(0xFFF2F2F2),
+                                    indicatorColor: Colors.blue,
+                                    iconColor: Colors.grey,
+                                    iconColorSelected: Colors.blue,
+                                    progressIndicatorColor: Colors.blue,
+                                    backspaceColor: Colors.blue,
+                                    skinToneDialogBgColor: Colors.white,
+                                    skinToneIndicatorColor: Colors.grey,
+                                    enableSkinTones: true,
+                                    showRecentsTab: true,
+                                    recentsLimit: 28,
+                                    replaceEmojiOnLimitExceed: false,
+                                    noRecents: Text(
+                                      'No Recents',
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.black26),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    tabIndicatorAnimDuration:
+                                        kTabScrollDuration,
+                                    categoryIcons: CategoryIcons(),
+                                    buttonMode: ButtonMode.MATERIAL),
+                              );
+                            },
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  Container(margin: const EdgeInsets.only(top: 20.0)),
-                  SizedBox(width: size.width * 0.8, child: nicknameField()),
-                  Container(margin: const EdgeInsets.only(top: 10.0)),
-                  SizedBox(width: size.width * 0.8, child: submitButton()),
-                ],
+                    Container(margin: const EdgeInsets.only(top: 20.0)),
+                    SizedBox(width: size.width * 0.8, child: nicknameField()),
+                    Container(margin: const EdgeInsets.only(top: 10.0)),
+                    SizedBox(width: size.width * 0.8, child: submitButton()),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
+          );
+        });
       },
     );
   }
